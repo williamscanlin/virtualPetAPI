e5a223197ac6f6bb27a0fba692fce6023ed7048b
williamscanlin <williamscanlin1@gmail.com>
2023-08-17 18:43:00 -0500

    as generated

---
+++ src/test/java/com/virtualPetAPI/VirtualPetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualPetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
